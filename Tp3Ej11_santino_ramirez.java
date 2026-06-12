6f57e71fb70e8e68720f16997708dfbaf8ab4b3e
santinoivanramirez09-byte <santinoivanramirez09@gmail.com>
2026-06-12 15:58:58 -0300

    Add files via upload

---
+++ Tp3Ej11_santino_ramirez.java
@@ -0,0 +1,14 @@
+package tp3.ej11_santino_ramirez;
+
+
+public class Tp3Ej11_santino_ramirez {
+
+ 
+    public static void main(String[] args) {
+    
+        System.out.println("Todos los numeros del 100 al 0 con decrementos de 23.");
+        for (int i = 100; i >= 0; i -= 23){
+            System.out.println(i);
+        }
+    }
+}
