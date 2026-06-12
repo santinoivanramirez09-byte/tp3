2359edecf5ebb21844e12e76050a860bca5119f6
santinoivanramirez09-byte <santinoivanramirez09@gmail.com>
2026-06-12 15:26:02 -0300

    Add files via upload

---
+++ Tp3Ej5_santino_ramirez.java
@@ -0,0 +1,17 @@
+package tp3.ej5_santino_ramirez;
+import java.util.Scanner;
+
+public class Tp3Ej5_santino_ramirez {
+
+    public static void main(String[] args) {
+        
+  Scanner lector = new Scanner(System.in);
+   int contador = 0;
+   int num = lector.nextInt();
+   int num2 = lector.nextInt();
+   while(num2 != num ){
+       num ++;
+       System.out.println(num);
+}
+}
+}
